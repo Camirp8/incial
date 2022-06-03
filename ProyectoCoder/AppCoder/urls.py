2b89feb37c7671d4a0ff53fa01206f93b685cd09
cemirp8 <camilar862@gmail.com>
2022-06-02 21:23:47 -0500

    Adelanto

---
+++ ProyectoCoder/AppCoder/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from AppCoder.views import profesores, curso
+
+urlpatterns = [
+    path ('curso/', curso),
+    path ('profesores/', profesores),
+]
---
+++ ProyectoCoder/AppCoder/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from AppCoder.models import Curso
+
+def curso(self):
+    curso=Curso(nombre="Desarrollo Web", camada=16740)
+    curso.save()
+    documento = f"Curso: {curso.nombre} - Camada: {curso.camada}"
+    return HttpResponse(documento)
+
+def profesores(self):
+    documento = f"Página de profesores"
+    return HttpResponse(documento)
